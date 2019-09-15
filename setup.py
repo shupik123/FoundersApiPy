3c6325f84fd64c48d5e6231c193936c1a3408f58
shupik <edo4eddie@gmail.com>
2019-09-15 19:04:38 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
 	name="founders",
 	version="0.1.0",
 	author="shupik",
-	author_email="shupik#2705",
 	description="A small package for the founders api",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
@@ -15,7 +14,6 @@ setuptools.setup(
 	packages=setuptools.find_packages(),
 	classifiers=[
 		"Programming Language :: Python :: 3",
-		"License :: GPL-3.0",
 		"Operating System :: OS Independent",
 	],
 	python_requires=">=3.6",
